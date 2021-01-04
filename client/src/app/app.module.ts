4636691169642adc03f9bd6522237a160de8a2cb
Prakash <prakash@atatus.com>
2021-01-04 23:06:59 +0530

    Added component home, login, forgetpassword, about

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    ForgetpasswordComponent,
+    LoginComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
